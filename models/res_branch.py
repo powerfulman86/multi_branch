8b420ed09f53a20320fdb7fd0e26e7f043c47d44
powerfulman86 <dba.ahmedmaher@gmail.com>
2022-01-02 11:16:21 +0200

    UPD

--- models/res_branch.py
+++ models/res_branch.py
@@ -8,7 +8,8 @@ class ResBranch(models.Model):
     _description = 'System Branches'
     _inherit = ['mail.thread', 'mail.activity.mixin', 'portal.mixin']
 
-    name = fields.Char('Branch')
+    name = fields.Char('Branch', translate=True, )
+    internal_reference = fields.Char(string="Internal Reference", required=False, )
     notes = fields.Text('Notes')
     active = fields.Boolean('Active', default=True, tracking=True,
                             help="Set active to false to hide the Branch without removing it.")
@@ -18,6 +19,31 @@ class ResBranch(models.Model):
         string='Branch Users',
         help='Users have added here, them will see any datas have linked to this Branch'
     )
+
+    _sql_constraints = [
+        (
+            "branch_code_uniq",
+            "unique(internal_reference)",
+            "Branch Code must be unique across the database!",
+        )
+    ]
+
+    def name_get(self):
+        result = []
+        for rec in self:
+            name = ('[%s] %s' % (rec.internal_reference, rec.name))
+            result.append((rec.id, name))
+        return result
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        super(ResBranch, self).name_search(name)
+        # args = args or []
+        domain = []
+        if name:
+            domain = ['|', ('name', operator, name), ('internal_reference', operator, name)]
+        recs = self.search(domain, limit=limit)
+        return recs.name_get()
 
 
 class ResUsers(models.Model):
